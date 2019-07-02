afe29ec0f7220a918698510cdd9d0dbe22667799
Michael Wonng <michael@wonng.com>
2019-07-02 23:13:58 +1000

    add some props

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+export interface HelloProps {
+  compiler: string;
+  framework: string;
+}
+
+const App: React.FC <HelloProps> = (props: HelloProps) => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and {props.compiler} and {props.framework}.
         </p>
         <a
           className="App-link"
